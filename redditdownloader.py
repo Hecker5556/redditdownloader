5aa5948e8977cd969fcc6817087086d1e2a53e47
unknown <christianmolicki@gmail.com>
2023-09-04 16:01:05 +0200

    add downloading for larger videos, proxy usage and caching

--- redditdownloader.py
+++ redditdownloader.py
@@ -1,4 +1,4 @@
-import aiohttp, asyncio, re, json, aiofiles
+import aiohttp, asyncio, re, json, aiofiles, os
 from html import unescape
 from datetime import datetime
 from tqdm.asyncio import tqdm
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 class redditdownloader:
     async def main(link: str):
         patternvideo = r'packaged-media-json=\"{&quot;playbackMp4s&quot;:((.*?)}}}]})'
+        patternmanifest = r'((https://v\.redd\.it/(?:.*?)/)HLSPlaylist\.m3u8\?(?:.*?))\"'
         headers = {
         'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Brave";v="116"',
         'Referer': 'https://www.reddit.com/',
@@ -18,6 +19,7 @@ class redditdownloader:
             async with session.get(link, headers=headers) as r:
                 rtext = await r.text()
             mainurls = re.findall(patternvideo, rtext)
+            manifesturls = re.findall(patternmanifest, rtext)
             if mainurls:
                 
                 mainurls = json.loads(unescape(mainurls[0][0]))
@@ -43,6 +45,27 @@ class redditdownloader:
                     temp[i[0]] = i[1]
                 postinfo = temp
             
+            elif patternmanifest:
+                manifesturl = manifesturls[0][0]
+                mainurl = manifesturls[0][1]
+                async with session.get(manifesturl) as response:
+                    responsetext = await response.text()
+                audioformats = {}
+                videoformats = {}
+                for i in responsetext.split('\n'):
+                    if i.startswith('#EXT-X-MEDIA:URI='):
+                        audiourlpattern = r'URI=\"(.*?)\"'
+                        audioidpattern = r'GROUP-ID=\"(.*?)\"'
+                        audioformats[re.findall(audioidpattern, i)[0]] = re.findall(audiourlpattern, i)[0]
+                videoformatspattern = r'#EXT-X-STREAM-INF:(?:[\s\S.]*?)AUDIO=\"(.*?)\"\n(.*?)\.m3u8'
+                matches = re.findall(videoformatspattern, responsetext)
+                for match in matches:
+                    videoformats[match[1]] = match[0]
+                videoformats = sorted(videoformats.items(), key=lambda x: int(x[0].split('_')[1]), reverse=True)
+                postinfo = []
+                for i in videoformats:
+                    postinfo.append((mainurl + i[0]+'.ts', mainurl + audioformats[i[1]].replace('.m3u8', '.aac')))
+                
                     
             else:
                 soup = BeautifulSoup(rtext, 'html.parser')
@@ -75,7 +98,7 @@ class redditdownloader:
                 else:
                     if value.get('contentlength')/(1024*1024) > maxsize:
                         continue
-                filename = f'redditvideo-{round(datetime.now().timestamp())}.mp4'
+                filename = f'redditvideo-{str(datetime.now().timestamp()).replace(".", "")}.mp4'
                 async with aiofiles.open(filename, 'wb') as f1:
                     async with aiohttp.ClientSession() as session:
                         async with session.get(value.get('url')) as r:
@@ -93,7 +116,7 @@ class redditdownloader:
             async with aiofiles.open(filename, 'wb') as f1:
                 async with aiohttp.ClientSession() as session:
                     async with session.get(postinfo) as r:
-                        progress = tqdm(total=int(r.headers.get('content-length')), unit='iB', unit_scale=True)
+                        progress = tqdm(total=int(r.headers.get('content-length'))  if not r.headers.get('Transfer-Encoding') == 'chunked' else None, unit='iB', unit_scale=True)
                         while True:
                             chunk = await r.content.read(1024)
                             if not chunk:
@@ -101,7 +124,7 @@ class redditdownloader:
                             await f1.write(chunk)
                             progress.update(len(chunk))
                         progress.close()
-        elif isinstance(postinfo, list):
+        elif isinstance(postinfo, list) and all(isinstance(item, str) for item in postinfo):
             filenames = []
             async with aiohttp.ClientSession() as session:
                 for index, url in enumerate(postinfo):
@@ -117,6 +140,62 @@ class redditdownloader:
                                 await f1.write(chunk)
                                 progress.update(len(chunk))
                             progress.close()
+        elif isinstance(postinfo, list) and all(isinstance(item, tuple) for item in postinfo):
+            filename = f"redditvideo-{str(datetime.now().timestamp()).replace('.', '')}.mp4"
+            async def download(link: str, filename: str, progress: tqdm, session: aiohttp.ClientSession):
+                async with session.get(link) as response:
+                    async with aiofiles.open(filename, 'wb') as f1:
+                        while True:
+                            chunk = await response.content.read(1024)
+                            if not chunk:
+                                break
+                            await f1.write(chunk)
+                            progress.update(len(chunk))
+                    return
+
+
+            async with aiohttp.ClientSession() as session:
+                for url, audiourl in postinfo:
+                    totalsize = 0
+                    while True:
+                        if not os.path.exists('proxycache.txt'):
+                            proxyurl = 'https://gimmeproxy.com/api/getProxy?curl=true'
+                            async with session.get(proxyurl) as proxies:
+                                proxy = await proxies.text()
+                        else:
+                            async with aiofiles.open('proxycache.txt', 'r') as f1:
+                                proxy = await f1.read()
+
+                        try:
+                            async with session.get(url, proxy = proxy, timeout=3) as r:
+                                totalsize += int(r.headers.get('content-length'))
+                            async with session.get(audiourl, proxy = proxy, timeout=3) as r:
+                                totalsize += int(r.headers.get('content-length'))
+                            async with aiofiles.open('proxycache.txt', 'w') as f1:
+                                await f1.write(proxy)
+                            break
+                        except:
+                            if os.path.exists('proxycache.txt'):
+                                os.remove('proxycache.txt')
+                            print('getting proxy failed, trying again...')
+                            continue
+                        
+                    if maxsize:
+                        if totalsize/(1024*1024) > maxsize:
+                            continue
+                    progress = tqdm(total=totalsize, unit='iB', unit_scale=True, colour='red')
+                    tasks = [download(url, url.split('/')[-1], progress, session), download(audiourl, audiourl.split('/')[-1], progress, session)]
+                    await asyncio.gather(*tasks)
+                    progress.close()
+                    process = await asyncio.subprocess.create_subprocess_exec(*f"ffmpeg -i {url.split('/')[-1]} -i {audiourl.split('/')[-1]} -c copy -map 0:v:0 -map 1:a:0 -y {filename}".split(), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+                    stdout, stderr = await process.communicate()
+                    if maxsize:
+                        if os.path.getsize(filename)/(1024*1024) >maxsize:
+                            continue
+                        else:
+                            break
+                    else:
+                        break
         return filename if not filenames else filenames
 if __name__ == '__main__':
     import argparse

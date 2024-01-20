2558d20721e6b0efff6093f822e703eb605f82ed
unknown <christianmolicki@gmail.com>
2024-01-20 18:50:33 +0100

    replace &#39; and &quot; with appropriate symbols in description

--- redditdownloader.py
+++ redditdownloader.py
@@ -43,6 +43,7 @@ class redditdownloader:
             thetext = {"caption": caption[0] if caption else caption, "description": "\n".join([d.lstrip().rstrip() for d in description]) if description else description}
             if thetext.get("description"):
                 thetext['description'] = re.sub(patternlinks, lambda match: match.group(1), thetext['description'])
+                thetext['description'] = thetext['description'].replace("&quot;", "\"").replace("&#39;", "'")
             postinfo = None
             if mainurls:
                 
